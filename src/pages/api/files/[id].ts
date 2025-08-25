9cb26853c575020c342a69ea3ce43e6f8bcebaee
Varppi <72181445+varppi@users.noreply.github.com>
2025-08-24 23:49:14 -0700

    1.1.1

--- src/pages/api/files/[id].ts
+++ src/pages/api/files/[id].ts
@@ -1,14 +1,16 @@
 import { PrismaClient } from "@/generated/prisma";
 import { isValidAltchaSolution } from "@/helpers/altcha";
+import { jwtVerify } from "@/helpers/jwt";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server"
 
 const prisma = new PrismaClient();
 
 export default async function GET(req: NextApiRequest, resp: NextApiResponse) {
-    if (process.env.DOWNLOAD_CAPTCHA === "true") {
-        const payload:{altcha: string} = {altcha: req.query.altcha === undefined || req.query.altcha === null ? "" : req.query.altcha.toString()};
-        if (!await isValidAltchaSolution(payload)) return resp.status(403).json({"message": "invalid captcha"});
+    const payload = await jwtVerify(req.cookies.token ?? "");
+    let userId: string = "";
+    if (payload !== null) {
+        userId = payload.id as string;
     }
 
     const file = await prisma.file.findFirst({
@@ -17,6 +19,10 @@ export default async function GET(req: NextApiRequest, resp: NextApiResponse) {
         },
     });
     if (!file) return resp.status(404);
+    if (process.env.DOWNLOAD_CAPTCHA === "true" && file.userId !== userId) {
+        const payload:{altcha: string} = {altcha: req.query.altcha === undefined || req.query.altcha === null ? "" : req.query.altcha.toString()};
+        if (!await isValidAltchaSolution(payload)) return resp.status(403).json({"message": "invalid captcha"});
+    }
   
     resp.setHeader('Content-Type', 'application/octet-stream');
     resp.setHeader('Content-Disposition', `attachment; filename="${file.fileName}"`);
